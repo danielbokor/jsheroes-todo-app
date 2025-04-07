234839e5daeedfa9739491d03909b7ee6ba1e60b
Daniel Bokor <contact@danielbokor.com>
2025-04-07 19:51:57 +0300

    feat: Streaming TodoList using Suspense Boundary; Fallback to TodoListSkeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import { TodoList } from "../components/TodoList";
+import { TodoList, TodoListSkeleton } from "@/components/TodoList";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="container mx-auto max-w-md">
       <h1 className="text-2xl font-bold mb-4">Todos:</h1>
-      <TodoList />
+      <Suspense fallback={<TodoListSkeleton />}>
+        <TodoList />
+      </Suspense>
     </main>
   );
 }
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -4,7 +4,13 @@ type Todo = {
   completed: boolean;
 };
 
+async function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 async function fetchTodos(): Promise<Todo[]> {
+  await delay(3000);
+
   const response = await fetch(
     "https://67a79752203008941f68094b.mockapi.io/todos",
     {
@@ -24,7 +30,7 @@ export async function TodoList() {
   return (
     <ul>
       {todos.map((todo) => (
-        <li key={todo.id}>
+        <li key={todo.id} className="flex items-center gap-2 p-2">
           <input
             type="checkbox"
             id={todo.id}
@@ -32,6 +38,19 @@ export async function TodoList() {
             readOnly
           />
           {todo.title}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export function TodoListSkeleton() {
+  return (
+    <ul>
+      {[...Array(5)].map((_, index) => (
+        <li key={index} className="flex items-center gap-2 p-2">
+          <div className="w-4 h-4 bg-gray-300 rounded-full animate-pulse"></div>
+          <div className="w-full h-4 bg-gray-300 rounded-full animate-pulse"></div>
         </li>
       ))}
     </ul>
