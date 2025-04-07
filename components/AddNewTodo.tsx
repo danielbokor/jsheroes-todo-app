624d036ec2ba275847e01a83147e3cc49f94dfa1
Daniel Bokor <contact@danielbokor.com>
2025-04-07 21:02:10 +0300

    feat: Using useFormStatus hook to enhance UX when handling form element components

--- components/AddNewTodo.tsx
+++ components/AddNewTodo.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 import { addTodo } from "../actions";
 
 export function AddNewTodo() {
@@ -26,12 +27,22 @@ export function AddNewTodo() {
           <p className="text-red-500">{state.error.title}</p>
         )}
       </div>
-      <button
-        type="submit"
-        className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors"
-      >
-        Add Todo
-      </button>
+      <SubmitButton />
     </form>
+  );
+}
+
+function SubmitButton() {
+  const { pending, data, method, action } = useFormStatus();
+
+  console.log(pending, data, method, action);
+
+  return (
+    <button
+      type="submit"
+      className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors"
+    >
+      {pending ? "Submitting..." : "Add Todo"}
+    </button>
   );
 }
